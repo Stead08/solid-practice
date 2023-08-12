819837ce59ce36b4738150281d4e529f8188fb31
Stead08 <inomatama08@gmail.com>
2023-08-12 21:05:33 +0900

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "extends": ["standard-with-typescript", "plugin:solid/typescript"],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+    "plugins": ["solid"]
+}
