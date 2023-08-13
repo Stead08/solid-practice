fe2379dd152b964409b70057b533996bd5ecdc8d
Stead08 <inomatama08@gmail.com>
2023-08-13 14:06:40 +0900

    configure ci and vitest config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-
-import { defineConfig } from 'vitest/config'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vitest/config";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solidPlugin()],
   // for the production build, enable this
   //define: {
   // +   'import.meta.vitest': 'undefined',
   // + },
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    includeSource: ['src/**/*.ts', 'src/**/*.tsx']
+    includeSource: ["src/**/*.ts", "src/**/*.tsx"],
+    deps: {
+      optimizer: {
+        web: {
+          include: ["src/**/*.ts", "src/**/*.tsx"]
+        }
+      }
+    }
   }
-})
+});
